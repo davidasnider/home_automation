449d48b6d17e81090aa07807a7efa5ae2df432e2
David Snider <david@davidsnider.org>
2022-06-05 20:57:00 -0600

    Add a script to update magic mirror temp

--- build_local.sh
+++ build_local.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 nerdctl build . -t sprinklermultiplier
+
+echo "Running the Sprinkler Multiplier"
 nerdctl run --env-file .env --rm sprinklermultiplier
+
+echo "Running Update Magic Mirror"
+nerdctl run --env-file .env --rm -it --net=host --entrypoint bash sprinklermultiplier -c "python3 /app/update_magic_mirror_temp.py"
